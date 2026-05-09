d8795e313bb4cbb886bb4cb144fb282c0682290c
Fullstack2711 <ibragimovzafar0001@gmail.com>
2026-05-09 14:21:28 +0500

    Remove deprecated Vercel output configuration files and assets to streamline deployment. This includes the removal of config.json, nitro.json, and various function files related to the fallback function, as well as static assets like favicon.ico and og-image.jpg.

--- .vercel/output/functions/__fallback.func/chunks/build/_tanstack-start-manifest_v-CPSPDwF4.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/ibrohimovzafar/Desktop/speed-launch-code/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CrIR68TZ.js"] }, "/": { filePath: "/Users/ibrohimovzafar/Desktop/speed-launch-code/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DHg7qe6c.js"] } }, clientEntry: "/assets/index-CrIR68TZ.js" });
-export {
-  tsrStartManifest
-};
